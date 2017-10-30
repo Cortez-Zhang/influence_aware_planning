d851ac1a88d09ce047285cda859d0877169d73cf
Ellis Ratner <ellis.ratner@gmail.com>
2017-10-29 18:14:45 -0700

    Cleaned up the code and added forward kinematics

--- interact_manipulation/src/interact_manipulation/jaco.py
+++ interact_manipulation/src/interact_manipulation/jaco.py
@@ -5,24 +5,36 @@ import rospy
 import moveit_commander
 import moveit_msgs.msg
 import geometry_msgs.msg
-from moveit_msgs.srv import GetPositionIK
-from moveit_msgs.msg import PositionIKRequest, DisplayTrajectory
+from moveit_msgs.srv import GetPositionIK, GetPositionFK
+from moveit_msgs.msg import PositionIKRequest, DisplayTrajectory, RobotState
+from std_msgs.msg import Header
 from visualization_msgs.msg import Marker
 from jaco_trajopt import JacoTrajopt
 
 
-WORLD_FRAME = '/world'
+class JacoInterface:
+    def __init__(self):
+        self.robot = moveit_commander.RobotCommander()
+        # print(self.robot.get_group_names())
+        # print(self.robot.get_current_state())
 
+        self.arm_group = moveit_commander.MoveGroupCommander('arm')
 
-def main():
-    marker_pub = rospy.Publisher('/visualization_marker', Marker, queue_size=10)
-    display_trajectory_pub = rospy.Publisher('/move_group/display_planned_path', DisplayTrajectory, queue_size=10)
+        self.marker_pub = rospy.Publisher('/visualization_marker', Marker, queue_size=10)
+        self.display_trajectory_pub = rospy.Publisher('/move_group/display_planned_path',
+                                                      DisplayTrajectory, queue_size=10)
 
-    # Set up the IK service
-    rospy.wait_for_service('/compute_ik')
-    compute_ik = rospy.ServiceProxy('/compute_ik', GetPositionIK)
+        # Set up the IK service
+        rospy.wait_for_service('/compute_ik')
+        self.compute_ik = rospy.ServiceProxy('/compute_ik', GetPositionIK)
 
-    def ik(pose_stamped, group_name='arm'):
+        # Set up the FK service
+        rospy.wait_for_service('/compute_fk')
+        self.compute_fk = rospy.ServiceProxy('/compute_fk', GetPositionFK)
+
+        self.planner = JacoTrajopt()
+
+    def ik(self, pose_stamped, group_name='arm'):
         """ Computes the inverse kinematics """
         req = PositionIKRequest()
         req.group_name = group_name
@@ -31,86 +43,120 @@ def main():
         req.avoid_collisions = False
 
         try:
-            res = compute_ik(req)
+            res = self.compute_ik(req)
             return res
         except rospy.ServiceException, e:
             print("IK service call failed: {}".format(e))
 
+    def fk(self, joints, links):
+        """ Computes the forward kinematics """
+        header = Header()
+        robot_state = RobotState()
+        robot_state.joint_state.name = self.planner.joint_names
+        robot_state.joint_state.position = joints
+        return self.compute_fk(header, links, robot_state)
+
+    def plan(self, start_pose, goal_pose):
+        """ Plan from the start pose to the end pose """
+
+        # Visualize the start pose
+        start_marker = Marker()
+        start_marker.header.frame_id = start_pose.header.frame_id
+        start_marker.header.stamp = rospy.get_rostime()
+        start_marker.ns = 'planning_start'
+        start_marker.id = 0
+        start_marker.type = Marker.SPHERE
+        start_marker.pose = start_pose.pose
+        start_marker.scale.x = 0.1
+        start_marker.scale.y = 0.1
+        start_marker.scale.z = 0.1
+        start_marker.color.r = 1.0
+        start_marker.color.g = 0.0
+        start_marker.color.b = 0.0
+        start_marker.color.a = 0.75
+        start_marker.lifetime = rospy.Duration(0)
+        self.marker_pub.publish(start_marker)
+
+        # Visualize the goal pose
+        goal_marker = Marker()
+        goal_marker.header.frame_id = goal_pose.header.frame_id
+        goal_marker.header.stamp = rospy.get_rostime()
+        goal_marker.ns = 'planning_goal'
+        goal_marker.id = 0
+        goal_marker.type = Marker.SPHERE
+        goal_marker.pose = goal_pose.pose
+        goal_marker.scale.x = 0.1
+        goal_marker.scale.y = 0.1
+        goal_marker.scale.z = 0.1
+        goal_marker.color.r = 0.0
+        goal_marker.color.g = 1.0
+        goal_marker.color.b = 0.0
+        goal_marker.color.a = 0.75
+        goal_marker.lifetime = rospy.Duration(0)
+        self.marker_pub.publish(goal_marker)
+
+        # Run IK to find the start and goal configuration from the goal pose
+        res = self.ik(goal_pose)
+        goal_config = [q for q in res.solution.joint_state.position[0:7]]
+
+        res = self.ik(start_pose)
+        start_config = [q for q in res.solution.joint_state.position[0:7]]
+
+        print("start config = {}, goal config = {}".format(start_config, goal_config))
+
+        # Plan a trajectory with trajopt
+        traj = self.planner.plan(start_config, goal_config)
+        print(traj)
+
+        return traj
+
+    def execute(self, traj, wait=True, display=True):
+        """ Execute the trajectory on the robot arm """
+
+        if display:
+            # Display the planned trajectory
+            display_traj = DisplayTrajectory()
+            display_traj.trajectory_start = self.robot.get_current_state()
+            display_traj.trajectory.append(traj)
+            self.display_trajectory_pub.publish(display_traj)
+
+        # Execute the trajectory
+        self.arm_group.execute(traj, wait=wait)
+
+
+def main():
     moveit_commander.roscpp_initialize(sys.argv)
     rospy.init_node('jaco_move_group')
 
-    robot = moveit_commander.RobotCommander()
-    print(robot.get_group_names())
-    print(robot.get_current_state())
+    jaco = JacoInterface()
 
-    arm_group = moveit_commander.MoveGroupCommander('arm')
     # print(arm_group.get_current_pose())
 
-    planner = JacoTrajopt()
+    # Move the arm to the initial configuration
+    start_pose = jaco.arm_group.get_current_pose()
+    print(start_pose)
 
-    # Visualize start and goal poses
-    start_pose = arm_group.get_current_pose()
+    initial_joints = [0.0, 3.1415, 0.0, 3.1415, 0.0, 3.1415, 0.0]
+    res = jaco.fk(initial_joints, links=['j2s7s300_end_effector'])
+    goal_pose = res.pose_stamped[0]
+    print(goal_pose)
 
-    start_marker = Marker()
-    start_marker.header.frame_id = start_pose.header.frame_id
-    start_marker.header.stamp = rospy.get_rostime()
-    start_marker.ns = 'planning_start'
-    start_marker.id = 0
-    start_marker.type = Marker.SPHERE
-    start_marker.pose = start_pose.pose
-    start_marker.scale.x = 0.1
-    start_marker.scale.y = 0.1
-    start_marker.scale.z = 0.1
-    start_marker.color.r = 1.0
-    start_marker.color.g = 0.0
-    start_marker.color.b = 0.0
-    start_marker.color.a = 0.75
-    start_marker.lifetime = rospy.Duration(0)
-    marker_pub.publish(start_marker)
+    traj = jaco.plan(start_pose, goal_pose)
+    print(traj)
 
-    # Construct the goal
-    goal_pose = start_pose
+    jaco.execute(traj)
+
+    tmp = raw_input("Enter to continue...")
+
+    # Move the arm to a new configuration
+    start_pose = jaco.arm_group.get_current_pose()
+
+    goal_pose = jaco.arm_group.get_current_pose()
     goal_pose.pose.position.x += 0.5
     goal_pose.pose.position.z -= 0.5
 
-    goal_marker = Marker()
-    goal_marker.header.frame_id = goal_pose.header.frame_id
-    goal_marker.header.stamp = rospy.get_rostime()
-    goal_marker.ns = 'planning_goal'
-    goal_marker.id = 0
-    goal_marker.type = Marker.SPHERE
-    goal_marker.pose = goal_pose.pose
-    goal_marker.scale.x = 0.1
-    goal_marker.scale.y = 0.1
-    goal_marker.scale.z = 0.1
-    goal_marker.color.r = 0.0
-    goal_marker.color.g = 1.0
-    goal_marker.color.b = 0.0
-    goal_marker.color.a = 0.75
-    goal_marker.lifetime = rospy.Duration(0)
-    marker_pub.publish(goal_marker)
-
-    # Run IK to find the goal configuration from the goal pose
-    res = ik(goal_pose)
-    print(res)
-
-    start_config = arm_group.get_current_joint_values()
-    goal_config = [q for q in res.solution.joint_state.position[0:7]]
-    # start_config = robot.get_current_state().joint_state.position
-    # goal_config = res.solution.joint_state.position
-    print("start config = {}, goal config = {}".format(start_config, goal_config))
-
-    # Plan a trajectory with trajopt
-    traj = planner.plan(start_config, goal_config)
-    print(traj)
-
-    # Display the planned trajectory
-    display_traj = DisplayTrajectory()
-    display_traj.trajectory_start = robot.get_current_state()
-    display_traj.trajectory.append(traj)
-    display_trajectory_pub.publish(display_traj)
-
-    arm_group.execute(traj, wait=False)
+    traj = jaco.plan(start_pose, goal_pose)
+    jaco.execute(traj)
 
     rospy.spin()
 
