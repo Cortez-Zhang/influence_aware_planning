37935fffa4ec1da31c17ddc98f33dbf6282d7389
Ellis Ratner <ellis.ratner@gmail.com>
2017-10-31 22:12:51 -0700

    1. Added a way to visualize bodies currently in the OpenRAVE environment used by the Trajopt planner 2. Added a way to load bodies from URDF files

--- interact_manipulation/src/interact_manipulation/jaco_trajopt.py
+++ interact_manipulation/src/interact_manipulation/jaco_trajopt.py
@@ -3,11 +3,15 @@
 import rospkg
 import json
 import time
+import random
+import math
+import numpy as np
 import trajoptpy
 import openravepy
 import rospy
 from moveit_msgs.msg import RobotTrajectory
 from trajectory_msgs.msg import JointTrajectoryPoint
+from visualization_msgs.msg import Marker, MarkerArray
 
 
 class JacoTrajopt:
@@ -21,9 +25,9 @@ class JacoTrajopt:
         print("Loading Jaco URDF from {} and SRDF from {}...".format(jaco_urdf_path,
                                                                      jaco_srdf_path))
 
-        urdf_module = openravepy.RaveCreateModule(self.env, 'urdf')
-        name = urdf_module.SendCommand("load {} {}".format(jaco_urdf_path,
-                                                           jaco_srdf_path))
+        self.urdf_module = openravepy.RaveCreateModule(self.env, 'urdf')
+        name = self.urdf_module.SendCommand("load {} {}".format(jaco_urdf_path,
+                                                                jaco_srdf_path))
         self.jaco = self.env.GetRobot(name)
         self.finger_joint_values = [0.0, 0.0, 0.0]
         self.joint_names = ['j2s7s300_joint_1',
@@ -34,11 +38,70 @@ class JacoTrajopt:
                             'j2s7s300_joint_6',
                             'j2s7s300_joint_7']
 
+    def load_body_from_urdf(self, path_to_urdf, transform=np.eye(4, 4)):
+        """ Load a body (non-robot object) from a URDF file into the OpenRAVE environment """
+        name = self.urdf_module.SendCommand("load {}".format(path_to_urdf))
+        body = self.env.GetKinBody(name)
+        body.SetTransform(transform)
+
+    def get_body_markers(self):
+        """ Returns a list of visualization_msgs/MarkerArray with all the links of each body in the environment """
+        body_markers = []
+
+        # Get all the bodies in the OpenRAVE environment
+        bodies = self.env.GetBodies()
+        for body in bodies:
+            print("Found body with name: {}".format(body.GetName()))
+            body_marker = MarkerArray()
+
+            # Choose a random color for this body
+            color_r = random.random()
+            color_g = random.random()
+            color_b = random.random()
+
+            # Create a separate marker for each link
+            for link in body.GetLinks():
+                print("  Link name: {}".format(link.GetName()))
+                link_transform = link.GetTransform()
+
+                link_marker = Marker()
+                link_marker.header.frame_id = '/world'
+                link_marker.header.stamp = rospy.get_rostime()
+                link_marker.ns = body.GetName() + '/link/' + link.GetName()
+                link_marker.id = 0
+                link_marker.type = Marker.SPHERE
+
+                pose = openravepy.poseFromMatrix(link_transform)
+
+                link_marker.pose.position.x = pose[4]
+                link_marker.pose.position.y = pose[5]
+                link_marker.pose.position.z = pose[6]
+                link_marker.pose.orientation.x = pose[1]
+                link_marker.pose.orientation.y = pose[2]
+                link_marker.pose.orientation.z = pose[3]
+                link_marker.pose.orientation.w = pose[0]
+
+                link_marker.scale.x = 0.2
+                link_marker.scale.y = 0.1
+                link_marker.scale.z = 0.1
+                link_marker.color.r = color_r
+                link_marker.color.g = color_g
+                link_marker.color.b = color_b
+                link_marker.color.a = 0.50
+                link_marker.lifetime = rospy.Duration(0)
+                body_marker.markers.append(link_marker)
+
+            body_markers.append(body_marker)
+
+        return body_markers
+
     def plan(self, start_config, goal_config):
         """ Plan from a start configuration to goal configuration """
         print("Planning from config {} to {}...".format(start_config,
                                                         goal_config))
         # print(self.jaco.GetDOFValues())
+        start_config[2] += math.pi  # TODO this seems to be a bug in OpenRAVE?
+        goal_config[2] += math.pi  # TODO this seems to be a bug in OpenRAVE?
         self.jaco.SetDOFValues(start_config + self.finger_joint_values)
         request = {
             "basic_info":
@@ -83,6 +146,7 @@ class JacoTrajopt:
         for i in range(traj.shape[0]):
             p = JointTrajectoryPoint()
             p.positions = traj[i, :].tolist()
+            p.positions[2] -= math.pi  # TODO this seems to be a bug in OpenRAVE?
             p.time_from_start = rospy.Duration(t)
             t += 0.5
 
