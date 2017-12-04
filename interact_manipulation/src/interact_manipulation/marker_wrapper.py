d69622d72fe9dfaf67a5e9cae1c60fb6dcec5198
sjhansen3 <stephenjhansen8@gmail.com>
2017-12-04 14:10:10 -0800

    Debub marker_wrapper

--- interact_manipulation/src/interact_manipulation/marker_wrapper.py
+++ interact_manipulation/src/interact_manipulation/marker_wrapper.py
@@ -6,36 +6,6 @@ from geometry_msgs.msg import Point, Pose, Quaternion
 marker_pub = rospy.Publisher('/visualization_marker', Marker, queue_size=10)
 marker_list = []
 
-# class MarkerWrapper():
-#     def __init__(self, position, **kwargs):
-#         self.kwarg = kwargs
-#         self.position = position
-    
-#     @property
-#     def marker(self):
-#         """Return a marker object"""
-#         #if position in positions
-#         num_markers = len(marker_list)+1
-
-#         waypoint_marker = MarkerWrapper(position, **kwargs)
-#         #rospy.loginfo("current pose {}".format(self.human_start_pose))
-#         waypoint_marker = Marker()
-#         waypoint_marker.header.frame_id = '/root' #TODO should this be root?
-#         waypoint_marker.header.stamp = rospy.get_rostime()
-#         waypoint_marker.ns = '/waypoint'
-#         waypoint_marker.id = num_markers
-#         waypoint_marker.type = Marker.SPHERE
-#         waypoint_marker.pose = numpy_position_tomessage(position)
-#         waypoint_marker.scale.x = self.kwargs[scale[0]
-#         waypoint_marker.scale.y = scale[1]
-#         waypoint_marker.scale.z = scale[2]
-
-#         waypoint_marker.color.r = color[0]
-#         waypoint_marker.color.g = color[1]
-#         waypoint_marker.color.b = color[2]
-#         waypoint_marker.color.a = 0.50
-#         waypoint_marker.lifetime = rospy.Duration(0) 
-
 """ A wrapper class for markers. Abstracts markers so that they can behave like a print statement
     Markers allow for visualization of positional data in rviz. Particularly useful for debug
     example usage: 
@@ -51,19 +21,15 @@ def show_position_marker(position, label = None, ident = 1, scale = (0.05,0.05,0
         @Param scale: (x,y,z) tuple (default 0.05)
     """
 
-    #TODO add a repeat = false param?
-    #if position in positions
-    #num_markers = len(marker_list)+1
-
-    #waypoint_marker = MarkerWrapper(position, **kwargs)
-    #rospy.loginfo("current pose {}".format(self.human_start_pose))
     waypoint_marker = Marker()
     waypoint_marker.header.frame_id = '/root' #TODO should this be root?
     waypoint_marker.header.stamp = rospy.get_rostime()
     waypoint_marker.ns = '/waypoint'
     waypoint_marker.id = ident
     waypoint_marker.type = Marker.SPHERE
+   
     waypoint_marker.pose = numpy_position_tomessage(position)
+   
     waypoint_marker.scale.x = scale[0]
     waypoint_marker.scale.y = scale[1]
     waypoint_marker.scale.z = scale[2]
@@ -73,12 +39,11 @@ def show_position_marker(position, label = None, ident = 1, scale = (0.05,0.05,0
     waypoint_marker.color.b = color[2]
     waypoint_marker.color.a = 0.50
     waypoint_marker.lifetime = rospy.Duration(0)
-    #if position[0] == -0.4:
-       # rospy.loginfo("waypoint_marker position: {} label: {} ident: {} \nobject {}".format(position,label,ident,waypoint_marker))
     marker_pub.publish(waypoint_marker)
+    #rospy.loginfo("before numpy_position_tomessage {}".format(position))
 
     if label:
-        position[2] = position[2]+.01   #TODO move the z position of the text up a bit
+        #position[2] = position[2]+.01   #TODO move the z position of the text up a bit
         text_marker = Marker()
         text_marker.header.frame_id = '/root' #TOOO should this be root?
         text_marker.header.stamp = rospy.get_rostime()
@@ -94,6 +59,8 @@ def show_position_marker(position, label = None, ident = 1, scale = (0.05,0.05,0
         text_marker.text = label
         text_marker.lifetime = rospy.Duration(0)
         marker_pub.publish(text_marker)
+        
+    #rospy.loginfo("after numpy_position_tomessage {}".format(position))
 
 def numpy_position_tomessage(np_position):
     """ Converts a numpy (3,) position to a Point message. orientation is set to 
