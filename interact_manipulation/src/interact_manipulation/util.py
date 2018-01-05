d9ed083ea5878513c6bf3117ac475d43d261c41d
Stephen Hansen <steveh@autresgrp.com>
2018-01-05 10:04:44 -0800

    Add direction and normalize to util.py

--- interact_manipulation/src/interact_manipulation/util.py
+++ interact_manipulation/src/interact_manipulation/util.py
@@ -1,5 +1,5 @@
 import inspect
-#import rospy
+import rospy
 
 def raiseNotDefined():
     fileName = inspect.stack()[1][1]
@@ -9,4 +9,25 @@ def raiseNotDefined():
     rospy.loginfo("*** Method not implemented: %s at line %s of %s" % (method, line, fileName))
 
 
+def direction(x, y):
+    """ Compute the direction from x to y
+        Params
+        ---
+        x: a numpy array
+        y: a numpy array
+        Returns
+        ---
+        normalized direction: numpy array of unit length to y from x
+    """
+    return (y - x)/np.linalg.norm(y - x + 1e-12)
 
+def normalize(beliefs):
+    """ Normalizes a list of scaler beliefs
+        Params
+        ---
+        beliefs: a list of scaler beliefs #TODO check this
+        Returns
+        ---
+        norm_beliefs: a np array of normalized beliefs
+    """
+    return np.asarray(beliefs)/np.sum(np.asarray(beliefs))
