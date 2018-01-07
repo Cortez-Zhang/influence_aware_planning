cd2c735e654e5eb5148f1f6bb6ee694faf340a24
sjhansen3 <stephenjhansen8@gmail.com>
2018-01-06 20:56:52 -0800

    Add set_params

--- interact_manipulation/src/interact_manipulation/util.py
+++ interact_manipulation/src/interact_manipulation/util.py
@@ -32,3 +32,26 @@ def normalize(beliefs):
         norm_beliefs: a np array of normalized beliefs
     """
     return np.asarray(beliefs)/np.sum(np.asarray(beliefs))
+
+def set_params(params, namespace=""):
+    """create an object from a list of params for use within an object
+       Warning: only for use with parameters which do not change at runtime
+       changes to parameters will not be made to the dict.
+       Params
+       --- 
+       params: a list of strings representing the parameter to be aquired
+       namespace: the namespace for which the pram exists e.g. /control_vars/P
+       Returns
+       ---
+       pram_dict: a dict populated with the parameters from the parameter server
+       Example usage
+       ---
+        params = ["dt","mass"]
+        namespace = "/human_model/"
+        self.params = util.set_params(params,namespace)
+    """
+    param_dict = {}
+    for param in params:
+        print(namespace+param)
+        param_dict[param] = rospy.get_param(namespace+param)
+    return param_dict
